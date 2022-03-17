be09e24460298e4fbf65b37bf8db93ac5377f457
Razoor <tiagofilipe122@gmail.com>
2022-03-17 00:02:42 +0000

    MASTER: ADD MENU/FILE/SPACE UTILITY FUNCTIONS

--- src/server/Server.java
+++ src/server/Server.java
@@ -11,6 +11,63 @@ public class Server{
         System.out.println("Choose data channel port: ");
         int dataPort = Integer.parseInt(in.readLine());
         new Thread(new ClientDataHandler(dataPort)).start();
+        menu();
+        if(String.equals("List Directory")){
+            fileAccess();
+        }
+        if(String.equals("Get Disk Space")){
+            getDiskSpace();
+        }
+    }
+    public static String menu(){
+        return """
+                \t\t\t\t       Client
+                \t\t\t\t\t   MENU
+                \t\t\t ________________________________
+                \t\t\t|                                |
+                \t\t\t|1 -> REGISTER  |
+                \t\t\t|                                |
+                \t\t\t|2 -> LOGIN    |
+                \t\t\t|                                |
+                \t\t\t|3 -> UPLOAD                 |
+                \t\t\t|                                |
+                \t\t\t|4 -> DOWNLOAD        |
+                \t\t\t|________________________________|
+                \s
+                \t\t\t5 - EXIT
+                                    \s""";
+    }
+    private static void showFiles(File curDir) {
+
+        File[] filesList = curDir.listFiles();
+        for (File f : filesList) {
+            if (f.isDirectory())
+                showFiles(f);
+            if (f.isFile()) {
+                System.out.println(f.getName());
+            }
+        }
+    }
+    public static void fileAccess(){
+        String PATH = "/remote/dir/server/";
+        String directoryName = PATH;
+        File directory = new File(directoryName);
+        if (!directory.exists()){
+            directory.mkdirs();
+            showFiles(directory);
+        }else{
+            showFiles(directory);
+        }
+    }
+
+    public static void getDiskSpace(File curDir){
+        long totalSpace = curDir.getTotalSpace();
+        long freeSpace = curDir.getFreeSpace();
+        long usedSpace = totalSpace - freeSpace;
+
+        System.out.println("Total space: "+totalSpace);
+        System.out.println("Used space: "+usedSpace);
+
     }
 }
 
